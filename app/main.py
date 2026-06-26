8738f40ea534732b2aaad4814367136ce8f56068
Bhagya-pujari <bhagyavbhagyav3@gmail.com>
2026-06-26 12:34:42 +0530

    fastapinotes

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from routers import company,job
+app=FastAPI()
+app.include_router(company.router)
+app.include_router(job.router)
+@app.get("/")
+def read_root():
+    return {"Hello":"World"}
+
+@app.get("/about")
+def read_about():
+    return{"about":"This is about Page"}
+
+@app.get("/contact")
+def read_contact():
+    return{"contact":"This is contact page"}
